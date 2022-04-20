8957c6f752120362c9cfed2d5e893d390c7b7bc1
kylefirman <kpfirman@live.unc.edu>
2022-04-20 17:48:16 -0400

    test fi

--- flip.js
+++ flip.js
@@ -2,3 +2,4 @@
 import { coinFlip, coinFlips, countFlips, flipACoin } from "./modules/coin.mjs";
 // Call the coinFlip function and put the return into STDOUT
 console.log(coinFlip());
+//random comment
